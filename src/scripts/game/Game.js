2779180bc4a644128892471e48ab88b3b1dd5d5c
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2024-03-16 11:30:26 +0400

    create-fild-component

--- src/scripts/game/Game.js
+++ src/scripts/game/Game.js
@@ -1,14 +1,17 @@
 import { App } from "../system/App";
 import { Scene } from "../system/Scene";
+import { Field } from "./Field";
 
 export class Game extends Scene {
-    create() {
-        this.createBackground();
-    }
-    createBackground() {
-        this.bg = App.sprite("bg");
-        this.bg.width = window.innerWidth;
-        this.bg.height = window.innerHeight;
-        this.container.addChild(this.bg);
-    }
+  create() {
+    this.createBackground();
+    const field = new Field(1, 1);
+    this.container.addChild(field.sprite);
+  }
+  createBackground() {
+    this.bg = App.sprite("bg");
+    this.bg.width = window.innerWidth;
+    this.bg.height = window.innerHeight;
+    this.container.addChild(this.bg);
+  }
 }
